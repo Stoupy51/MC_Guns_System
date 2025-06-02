e6d26b83d603d2b5573bafba6f88120420636005
Stoupy51 <stoupy51@gmail.com>
2025-06-02 20:23:15 +0200

    refactor: ♻️ Renamed right_click folder to weapon

--- user/link.py
+++ user/link.py
@@ -1,13 +1,13 @@
 
 # Imports
 from user.functional.main import main as main_datapack
-from user.functional.right_click import main as main_right_click
+from user.functional.main import main as main_weapon
 from user.functional.tick import main as main_tick
 
 
 # Main function is run just before making finalyzing the build process (zip, headers, lang, ...)
 def main(config: dict) -> None:
     main_datapack(config)
-    main_right_click(config)
+    main_weapon(config)
     main_tick(config)
 
